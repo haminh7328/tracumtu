62fc3770f54e5e17dbdfd470f4739125a75736f8
minhhc <minhhc@deha-soft.com>
2020-06-09 13:22:32 +0700

    draf ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Paper, IconButton, InputBase, Grid } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '20px',
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+  logo: {
+    textAlign: 'center'
+  },
+  searchResult: {
+    padding: '30px 20px',
+    display: 'flex',
+    marginTop: '20px',
+  },
+  titleSearchResult: {
+    margin: '0px',
+    marginBottom: '30px',
+    borderBottom: '1px solid #8e8e8e',
+    width: '100%',
+  },
+  result: {
+    borderRight: '1px solid #8e8e8e',
+    borderRadius: '0px',
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <h1 className={classes.logo}> Tra Cụm Từ . Com </h1>
+      <Paper component="form" className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder="Gõ cụm từ bạn muốn, ví dụ: as far as, the crazy part,..."
+          inputProps={{ 'aria-label': 'search google maps' }}
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+
+      <Paper className={classes.searchResult}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <h3 className={classes.titleSearchResult}>KẾT QUẢ TÌM KIẾM</h3>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper elevation={0} className={classes.result}>3</Paper>
+          </Grid>
+          <Grid item xs={9}>
+            <Paper elevation={0}>9</Paper>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 }
 
